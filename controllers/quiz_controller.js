6b37b09b73cf7f765be4e7b5f96ba40381262a12
Ismael Fernández <ismez013@gmail.com>
2015-06-14 18:09:09 +0200

    0.8.0
    Sequelize actualizado a la versión 2.0.0.
    
    Ahora es posible agregar preguntas y editar o borrar las existentes.
    
    Corregidos algunos typos.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -19,13 +19,13 @@ exports.index = function(req, res) {
 									req.query.search.replace(/\s/g, '%') +
 									'%']}).then(
 			function(quizes) {	
-				res.render('quizes/index.ejs', {quizes: quizes});
+				res.render('quizes/index.ejs', {quizes: quizes, errors: []});
 			}
 		).catch(function(error) { next(error);});
 	} else {
 		models.Quiz.findAll().then(
 			function(quizes) {	
-				res.render('quizes/index.ejs', {quizes: quizes});
+				res.render('quizes/index.ejs', {quizes: quizes, errors: []});
 			}
 		).catch(function(error) { next(error);});
 	}
@@ -33,10 +33,10 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
-// Get /quizes/:id/answer
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
 	var re = new RegExp('\^' +
 							req.quiz.respuesta
@@ -50,5 +50,69 @@ exports.answer = function(req, res) {
 		resultado = 'Correcto';
 	}
 	res.render( 'quizes/answer',
-				{quiz: req.quiz, respuesta: resultado});
+				{quiz: req.quiz, respuesta: resultado, errors: []});
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( 		//Crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+
+	quiz
+		.validate()
+		.then(
+			function(err) {
+				if (err) {
+					res.render('quizes/new', {quiz: quiz, errors: err.errors});
+				} else {
+					quiz 	//save: guarda en DB campos pregunta y respuesta
+						.save({fields: ["pregunta", "respuesta"]})
+						.then(function(){res.redirect('/quizes')})
+				}
+			}
+		);		//Redirección HTTP (URL relativo) lista de preguntas
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; 		//autoload de instancia de quiz
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	
+	req.quiz
+		.validate()
+		.then(
+			function(err) {
+				if (err) {
+					res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+				} else {
+					req.quiz
+						.save({fields: ["pregunta", "respuesta"]})
+						.then(
+							function(){
+								res.redirect('/quizes');
+							}
+						);
+				}
+			}
+		);		//Redirección HTTP (URL relativo) lista de preguntas
+};
+
+// DELETE /quizes/:id/edit
+exports.destroy = function(req, res) {
+	req.quiz.destroy().then( function() {
+		res.redirect('/quizes');
+	}).catch(function(error) { next(error)});
 };
